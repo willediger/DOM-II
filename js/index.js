d650013f135df3f1f6a952cbcb39672e8c3e9e49
will Ediger <will.ediger@gmail.com>
2019-02-01 19:45:58 -0600

    added descriptive comments and improved spacing

--- js/index.js
+++ js/index.js
@@ -1,9 +1,14 @@
+//on click of menu item, change the font family of that element to verdana.
 let header = document.querySelector('.main-navigation');
 header.addEventListener('click', (event) => {
   event.target.style.fontFamily = 'Verdana';
   event.preventDefault();
 });
 
+
+
+//on click of logo inside header, change color of text to purple.
+//does not change the font to verdana due to stopPropagation.
 let logo = document.querySelector('.logo-heading');
 logo.addEventListener('click', (event) => {
   event.target.style.color = 'purple';
@@ -11,17 +16,26 @@ logo.addEventListener('click', (event) => {
 });
 
 
+
+//on doubleclick of element in intro, "welcome to fun bus", image above, and paragraph below, change background color to orange
 let intro = document.querySelector('.intro');
 intro.addEventListener('dblclick', (event) => {
   event.target.style.background = 'orange';
 });
 
+
+
+//on doubleclick of image in intro (above welcome to fun bus), change width to 80% and scale vertically.
+//does not change background color to orange due to stopPropagation.
 let introImg = intro.querySelector('img');
 introImg.addEventListener('dblclick', (event) => {
   event.target.style.width = '80%';
   event.stopPropagation();
 });
 
+
+
+//on mousover of the h2 in the first section "let's go", change font size to 4rem. on mouseout, change it back
 let originalFirstSectionH2FontSize;
 let firstSectionH2 = document.querySelector('.content-section h2');
 firstSectionH2.addEventListener('mouseover', (event) => {
@@ -33,42 +47,62 @@ firstSectionH2.addEventListener('mouseout', (event) => {
 });
 
 
+
+//on using scroll wheel while hovered over the first paragraph under "let's go", change the color to blue
+//first section paragraphs ("let's" go section)
 let firstSectionPs = document.querySelectorAll('.content-section p');
 firstSectionPs[0].addEventListener('wheel', (event) => {
   event.target.style.color = 'blue';
 }, {passive: true}); //passive so we don't get an error that we're trying to override default scrolling behavior.
 
+
+
+//on mousedown on the second paragraph under "let's go", change the color to green
 firstSectionPs[1].addEventListener('mousedown', (event) => {
   event.target.style.color = 'green';
 });
 
 
-let secondSection = document.querySelector('.content-section.inverse-content');
 
+//once the image in the second section has loaded, add a black border around it.
+//second section = "adventure awaits". image is to the left of that text.
+let secondSection = document.querySelector('.content-section.inverse-content');
 let secondSectionImg = secondSection.querySelector('img');
 secondSectionImg.addEventListener('load', (event) => {
   event.target.style.border = '4px solid black';
 });
 
-let secondSectionPs = secondSection.querySelectorAll('p');
 
+
+//on both the paragraphs in the second section ("adventure awaits"), when right clicking add a red border.
+//we also stop the context menu from coming up.
+let secondSectionPs = secondSection.querySelectorAll('p');
 secondSectionPs.forEach(e => e.addEventListener('contextmenu', (event) => {
   event.target.style.border = '4px solid red';
   event.preventDefault(); //prevents right click from bringing in context menu
 }));
 
 
+
+//on both the paragraphs in the second section ("adventure awaits"), when left clicking add a blue border.
 secondSectionPs.forEach(e => e.addEventListener('click', (event) => {
   event.target.style.border = '4px solid blue';
 }));
 
 
-let contentDestinationH2 = document.querySelector('.content-destination h2');
 
+//when right clicking or auxilliary clicking, add an orange border around the h2 in "pick your destination" section
+//the h2 in the "pick your destination" section
+let contentDestinationH2 = document.querySelector('.content-destination h2');
 contentDestinationH2.addEventListener('auxclick', (event) => {
   event.target.style.border = '4px solid orange';
 });
 
+
+
+//for the paragraph in the "pick your destination" section, we allow it to receive focus and accept keydown events.
+//on focus, we stop the normal blue outline from happening
+//on keydown after focus, we give it a cyan border
 let contentDestinationP = document.querySelector('.content-destination p');
 
 //setting tab index so it can receive focus for receiving keydown events.
@@ -79,7 +113,7 @@ contentDestinationP.addEventListener('focus', (event) => {
   event.target.style.outline = 'none';
 });
 
-//color cycan border when keydown after focused.
+//color cyan border when keydown after focused.
 contentDestinationP.addEventListener('keydown', (event) => {
   event.target.style.border = '4px solid cyan';
 });
